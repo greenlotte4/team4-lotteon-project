1edfd80d71e980416a91094a8381f85b8721eb20
gyuchanJeon <ian810900@gmail.com>
2024-10-18 17:54:16 +0900

    product options register form

---
+++ src/main/java/com/lotte4/controller/pagecontroller/gyubooke/ProductRestController.java
@@ -0,0 +1,14 @@
+package com.lotte4.controller.pagecontroller.gyubooke;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+public class ProductRestController {
+
+
+
+}
