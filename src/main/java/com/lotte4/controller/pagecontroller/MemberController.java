376abc6f63ec3faed27c05773ca0bbdf7c6f5aaf
ekkang2 <rkddmsrud27@gmail.com>
2024-10-21 17:07:58 +0900

    24/10/21 17:07 terms

--- src/main/java/com/lotte4/controller/pagecontroller/MemberController.java
+++ src/main/java/com/lotte4/controller/pagecontroller/MemberController.java
@@ -1,21 +1,28 @@
 package com.lotte4.controller.pagecontroller;
 
+import com.lotte4.dto.TermsDTO;
 import com.lotte4.dto.UserDTO;
+import com.lotte4.entity.Terms;
+import com.lotte4.service.TermsService;
 import com.lotte4.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Log4j2
 public class MemberController {
 
     private final UserService userService;
+    private final TermsService termsService;
 
-    public MemberController(UserService userService) {
+    public MemberController(UserService userService, TermsService termsService) {
         this.userService = userService;
+        this.termsService = termsService;
     }
 
     @GetMapping("/member/login")
@@ -30,8 +37,15 @@ public class MemberController {
         return "/member/join";
     }
 
+    // 약관 동의
     @GetMapping("/member/signup")
-    public String signup(){
+    public String signup(@RequestParam("type") int type, Model model) {
+
+        TermsDTO termsDTO = termsService.selectTerms();
+        model.addAttribute("termsDTO", termsDTO);
+        model.addAttribute("type", type);
+        log.info("termsDTO : " + termsDTO);
+
         return "/member/signup";
     }
 
