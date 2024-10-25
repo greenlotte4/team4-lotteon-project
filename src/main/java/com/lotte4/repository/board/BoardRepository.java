5cbf9c43c03ca25ab94ba60c27ee060390c729b6
subin3578 <ghkdtnqls95@gmail.com>
2024-10-25 17:20:21 +0900

    board add paging

--- src/main/java/com/lotte4/repository/board/BoardRepository.java
+++ src/main/java/com/lotte4/repository/board/BoardRepository.java
@@ -2,6 +2,8 @@ package com.lotte4.repository.board;
 
 import com.lotte4.entity.Board;
 import com.lotte4.entity.BoardCate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +15,5 @@ import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-    List<Board> findByType(String type);
+    Page<Board> findByType(String type, Pageable pageable);
 }
