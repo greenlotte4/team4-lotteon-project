b4e881f71dcc5421d8cbed5fd281a5e763dc96de
subin3578 <ghkdtnqls95@gmail.com>
2024-10-31 17:22:57 +0900

    remove error

--- src/main/java/com/lotte4/controller/pagecontroller/admin/member/AdminMemberController.java
+++ src/main/java/com/lotte4/controller/pagecontroller/admin/member/AdminMemberController.java
@@ -106,7 +106,6 @@ public class AdminMemberController {
         memberInfoService.updateMemberGrades(memberGrades);
         return ResponseEntity.ok().build(); // 200 OK 응답 반환
     }
-     s
     @ResponseBody
     @GetMapping("/member/point")
     public ResponseEntity<Page<PointDTO>> pointfinder (@RequestParam(required = false) String searchType,
