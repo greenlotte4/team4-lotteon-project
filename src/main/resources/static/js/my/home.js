96169d9516bb298fa45c2e08f09f7d24b35cc71c
ianjeon <ian810900@gmail.com>
2024-11-06 22:24:14 +0900

    마이페이지 모달창 90% 완료

--- src/main/resources/static/js/my/home.js
+++ src/main/resources/static/js/my/home.js
@@ -1,23 +1,24 @@
 'use strict'
 
-const orderLinks = document.querySelector('.orderDetailBtn');
+const orderLink = document.querySelector('.orderDetailBtn');
 const orderReviewBtns = document.getElementsByClassName('orderReviewBtn');
 const orderInquireBtns = document.getElementsByClassName('orderInquireBtn');
 const orderSellerBtns = document.getElementsByClassName('orderSellerBtn');
 const orderAcceptBtns = document.getElementsByClassName('orderAcceptBtn');
 const orderReturnBtns = document.getElementsByClassName('orderReturnBtn');
 const orderChangeBtns = document.getElementsByClassName('orderChangeBtn');
+const closeBtns = document.querySelectorAll('.closeBtn');
+const cancelBtns = document.querySelectorAll('.cancelBtn');
 
 
-for (let i = 0; i < orderLinks.length; i++) {
-    orderLinks[i].addEventListener('click', function (event) {
-        event.preventDefault(); // a 태그의 기본 동작(링크 이동)을
-        const modal = document.getElementById('productModal');
-        if (modal) {
-            modal.classList.remove('Modalhidden'); // Modalhidden 클래스 제거
-        }
-    });
-}
+orderLink.addEventListener('click', function (event) {
+    event.preventDefault(); // a 태그의 기본 동작(링크 이동)을
+    const modal = document.getElementById('productModal');
+    if (modal) {
+        modal.classList.remove('Modalhidden'); // Modalhidden 클래스 제거
+    }
+})
+
 
 for (let i = 0; i < orderReviewBtns.length; i++) {
     orderReviewBtns[i].addEventListener('click', function (event) {
@@ -76,6 +77,26 @@ for (let i = 0; i < orderChangeBtns.length; i++) {
         const modal = document.getElementById('ChangeModal');
         if (modal) {
             modal.classList.remove('Modalhidden'); // Modalhidden 클래스 제거
+        }
+    });
+}
+
+for (let i = 0; i < closeBtns.length; i++) {
+    closeBtns[i].addEventListener('click', function (event) {
+        event.preventDefault(); // a 태그의 기본 동작(링크 이동)을 막음
+        const displayStatus = event.target.closest('.myInfo_modal');
+        if (displayStatus) {
+            displayStatus.classList.add('Modalhidden'); // Modalhidden 클래스 추가
+        }
+    });
+}
+
+for (let i = 0; i < cancelBtns.length; i++) {
+    cancelBtns[i].addEventListener('click', function (event) {
+        event.preventDefault(); // a 태그의 기본 동작(링크 이동)을 막음
+        const displayStatus = event.target.closest('.myInfo_modal');
+        if (displayStatus) {
+            displayStatus.classList.add('Modalhidden'); // Modalhidden 클래스 추가
         }
     });
 }
